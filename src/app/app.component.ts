ead55f893eef3121705505d177c1279c507c9c97
paugil <pauline.gil@hotmail.com>
2018-03-15 12:26:49 +0100

    achat produit [1]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,33 @@ export class AppComponent {
   title = 'app';
   world: World = new World();
   server: string;
+  Tabmulti = ["x1", "x10", "x100", "xMax"];
+  qtmulti = this.Tabmulti[0];
+ 
 
   constructor(private service: RestserviceService) {
     this.server = service.getServer();
     service.getWorld().then(world => { 
       this.world = world;
-    }
-  );
+    });
   }
+  
+  onProductionDone(p : Product) {
+    this.world.money += p.revenu;
+    this.world.score += p.revenu;
+  }
+
+  defQmulti(){
+    if (this.qtmulti === "x1"){
+      this.qtmulti = this.Tabmulti[1];
+    } else if (this.qtmulti === "x10"){
+      this.qtmulti = this.Tabmulti[2];
+    } else if (this.qtmulti === "x100"){
+      this.qtmulti = this.Tabmulti[3];
+    } else if (this.qtmulti === "xMax"){
+      this.qtmulti = this.Tabmulti[0];
+    }
+  }
+
+ 
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, ViewChild, Output, EventEmitter } from '@angular/core';
 import { World, Pallier, Product } from '../world';
 import { RestserviceService } from '../restservice.service';
+declare var require: any;
 
 @Component({
   selector: 'app-product',
@@ -11,6 +12,7 @@ import { RestserviceService } from '../restservice.service';
 
 export class ProductComponent implements OnInit {
  
+ 
   @ViewChild('bar') progressBarItem
 
   ProgressBar = require("progressbar.js");
@@ -18,14 +20,23 @@ export class ProductComponent implements OnInit {
   progress = 1;
   url : String;
   product : Product;
+  money : number;
+  etatProduc = 0;
+  @Input()
   timeleft;
   lastupdate;
-  @Input()
-  // @Output() notifyProduction: EventEmitter<Product> = new EventEmitter<Product>();
+  _qtmulti: string;
+  @Output() notifyProduction: EventEmitter<Product> = new EventEmitter<Product>();
   
+  
+  @Input()
   set prod(value : Product){
     this.product = value;
-    console.log(this.product)
+  }
+
+  @Input()
+  set worldMoney(value : number){
+    this.money = value;
   }
 
   constructor(private service : RestserviceService) { 
@@ -40,8 +51,9 @@ export class ProductComponent implements OnInit {
 
   startFabrication() {
     if(this.product.quantite>=1){
+      this.etatProduc = 1;
       this.progressbar.animate(1, { duration: this.product.vitesse });
-      // this.progressbar.set(this.progress);
+      this.progressbar.set(this.progress);
     } 
     this.timeleft = this.product.vitesse;
     this.lastupdate = Date.now()
@@ -50,11 +62,31 @@ export class ProductComponent implements OnInit {
   calcScore(): any {
     if(this.timeleft <= 0){
       this.timeleft = 0;
-      this.progressbar.set(0);
+      this.progressbar.set(0);  
+      if (this.etatProduc == 1){
+        this.notifyProduction.emit(this.product);
+        this.etatProduc = 2;
+      }
     } else {
       this.timeleft = Date.now() - this.lastupdate - this.timeleft;
     }
   }
 
+  @Input()
+  set qtmulti(value: string) {
+    console.log(value);
+    this._qtmulti = value;
+    if (this._qtmulti && this.product) 
+        this.calcMaxCanBuy();
+  }
+
+  calcMaxCanBuy(): any {
+    //en fonction de la valeur _qtmulti on calcule le cout de l'achat, penser à afficher la valeur pour le parent
+    var qMax = 0;
+    while (this.money > this.prod.cout * ((1-this.prod.croissance ** (qMax+1))/(1-this.prod.croissance))){
+      qMax += 1 ;
+    }
+    console.log(qMax)
+  }
 
 }
